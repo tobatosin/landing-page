1d29f2be0a71f993a18b0eaeec71affa02218f00
Negsvillian12 <113173084+Negsvillian12@users.noreply.github.com>
2023-09-09 17:59:56 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-
-
-
-    
+//  CURRENT DAY OF THE WEEK   
 var today = new Date();
 var day =today.getDay();
 var daylist = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday' , 'Friday', 'Saturday'];
@@ -9,7 +6,7 @@ var daylist = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday' , 'Friday'
 
 document.getElementById("dow").innerHTML= daylist[day];
     
-
+// CURRENT UTC TIME 
 var currentDate = new Date();
 var milliseconds = currentDate.getTime();
 
